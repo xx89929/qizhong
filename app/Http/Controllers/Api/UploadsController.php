3d15759f325b93d0fcf33d33f5c5718d0a3aeb70
lth <xx89929@163.com>
2018-08-10 01:09:33 +0800

    '0.1.9'

--- app/Http/Controllers/Api/UploadsController.php
+++ app/Http/Controllers/Api/UploadsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class UploadsController extends Controller
 {
