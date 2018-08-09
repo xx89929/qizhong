2267931a9e031f3a19cdb5024e073bbdf5d00356
lth <xx89929@163.com>
2018-08-09 20:11:06 +0800

    '0.0.4'

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,27 @@
 
-$(window).scroll(function () {
-    var mobileScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-
-    console.log('判断手机',isMobile());
-    console.log(mobileScrollTop);
-    var topp = $(document).scrollTop();
+$(function () {
     if(isMobile()){
-        alert('是手机');
         $('.case-index-box').css('animation-play-state','running');
         $('.flow-num,.flow-num div').css('animation-play-state','running');
         $('.flow-content').css('animation-play-state','running');
         $('.fb-button-path').css('animation-play-state','running');
-
-    }else{
-        if(topp > 553){
-            $('.case-index-box').css('animation-play-state','running');
-        }
-        if(topp > 2200){
-            $('.flow-num,.flow-num div').css('animation-play-state','running');
-            $('.flow-content').css('animation-play-state','running');
-        }
-
-        if(topp > 2750){
-            $('.fb-button-path').css('animation-play-state','running');
-        }
     }
+})
+
+
+$(window).scroll(function () {
+    var mobileScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    var topp = $(document).scrollTop();
+    if(topp > 553){
+        $('.case-index-box').css('animation-play-state','running');
+    }
+    if(topp > 2200){
+        $('.flow-num,.flow-num div').css('animation-play-state','running');
+        $('.flow-content').css('animation-play-state','running');
+    }
+
+    if(topp > 2750){
+        $('.fb-button-path').css('animation-play-state','running');
+    }
+
 })
