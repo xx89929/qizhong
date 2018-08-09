e6645c14e85755ba1604ad0c68f2b145bb6d108d
lth <xx89929@163.com>
2018-08-09 23:35:08 +0800

    '0.1.5'

--- app/Http/Controllers/APi/UploadsController.php
+++ app/Http/Controllers/APi/UploadsController.php
@@ -13,15 +13,6 @@ class UploadsController extends Controller
     public function editUploads(Request $request){
 
         $path['errno'] = 0;
-//        $file = $request->file('wang_file');
-//        $extension = $file->extension();
-//        $store_result = $file->store('images');
-//        $output = [
-//            'extension' => $extension,
-//            'store_result' => $store_result
-//        ];
-//        $res['data'] = $output;
-
         $path_img = $request->file('wang_file')->store('public');
 
         $path['data'] = [env('APP_URL').Storage::url($path_img)];
