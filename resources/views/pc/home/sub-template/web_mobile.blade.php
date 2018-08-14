4f1b5d2e5f855ceccd91d29c0d19572261e12e7a
lth <xx89929@163.com>
2018-08-14 13:32:16 +0800

    'templates'

---
+++ resources/views/pc/home/sub-template/web_mobile.blade.php
@@ -0,0 +1,8 @@
+<section class="web_mobile-template-warp">
+    <img title="移动端网站" class="img-fluid" src="{{asset('img/web_mobile.jpg')}}">
+</section>
+
+@section('csss')
+    @parent
+    <link rel="stylesheet" href="{{asset('scss/template.css')}}">
+@endsection
---
+++ resources/views/pc/home/sub-template/wechat_official.blade.php
@@ -0,0 +1,21 @@
+<section class="common-template-warp">
+    <div class="container">
+        <div class="row ">
+            <div class="col-6">
+                <img title="微信公众号demo" class="img-fluid" src="{{asset('img/wxgzh.jpg')}}">
+            </div>
+            <div class="col-6 common-template-content">
+                <h1>微信公众号开发</h1>
+                <p>
+                    微信推广定位精准，微信公众账号让粉丝的分类更加多样化和多元化，可以通过后台的用户分组和地域控制实现精准的消息推送，也就是说可以把你的不同的粉丝放在不同的分类下面。在信息发送的时候，可以针对用户的特点实现精准的消息推送。
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+@section('csss')
+    @parent
+    <link rel="stylesheet" href="{{asset('scss/template.css')}}">
+@endsection
